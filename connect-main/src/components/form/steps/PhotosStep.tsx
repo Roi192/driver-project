a3f9f51565f18e2da4d24ab5d61e2a927a7c8254
Roi192 <roimoshe92@gmail.com>
2026-03-08 14:47:41 +0200

    Rename training thumbnail to png

--- connect-main/src/components/form/steps/PhotosStep.tsx
+++ connect-main/src/components/form/steps/PhotosStep.tsx
@@ -18,6 +18,7 @@ export function PhotosStep() {
     register("photos");
   }, [register]);
 
+  // Only revoke blob URLs on component unmount
   useEffect(() => {
     return () => {
       Object.values(previewUrls).forEach((url) => {
@@ -26,7 +27,8 @@ export function PhotosStep() {
         }
       });
     };
-  }, [previewUrls]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const openCamera = (photoId: string) => {
     const input = fileInputRefs.current[photoId];
